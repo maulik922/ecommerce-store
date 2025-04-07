146d6939b1d657d492bfab80deb0a29c63a500bc
indra2821 <109273523+indra2821@users.noreply.github.com>
2025-04-07 17:49:16 +0530

    Delete frontend/src/test.js

--- frontend/src/test.js
+++
@@ -1,9 +0,0 @@
-// BadCode.js
-function unusedFunction() {
-  var x = 42; // unused variable
-}
-
-const duplicate = 'oops';
-const duplicate = 'again'; // duplicate declaration
-
-console.log("This line is toooooooooooooooooooooooooooooooooooooooooooooooooooooo long to be good!");
